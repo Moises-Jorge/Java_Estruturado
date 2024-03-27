4a0e6890bb058daeaa7f67d63234b236b516db02
Moisés Jorge <moisesjorge30@gmail.com>
2024-03-28 00:42:46 +0100

    Testando codigo: Mexendo no arquivo de teste para testar Array e ArrayList

--- TiposDados&Variaveis/src/App.java
+++ TiposDados&Variaveis/src/App.java
@@ -1,10 +1,42 @@
+//import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     // Criando o objecto de leitura (Global)
     static Scanner entrada = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
+        
 
+        /* BUMBANDO COM ARRAY PURO
+         String[] nomes = new String[4];
+
+        for (int posicao = 0; posicao < nomes.length; posicao++) {
+            System.out.format("Digite o %d nome: ", posicao + 1);
+            nomes[posicao] = entrada.nextLine();
+        }
+        
+        System.out.println();
+        for (int posicao = 0; posicao < nomes.length; posicao++) {
+            System.out.print(nomes[posicao] + " ");
+        } */
+
+        /* BUMBANDO COM ARRAY DE TAMANHO DINAMICO (ArrayList)
+        ArrayList<String> lista_nomes = new ArrayList<>();
+        String nome = "";
+
+        System.out.println("Digite os nomes (Digite 'fim' para parar):");
+        while (!(nome = entrada.nextLine()).equals("fim")) {
+            lista_nomes.add(nome);
+        }
+
+        System.out.println("\nOs elementos da lista sao:");
+        for (String name : lista_nomes) {
+            System.out.println(name);
+        }
+
+        entrada.close(); */
+
+        /* VARIAVEIS, TIPO DE DADOS E LEITURA 
         String nome, descricao, aux;
         Integer idade;
         Double peso;
@@ -23,6 +55,6 @@ public class App {
         System.out.print("Como te descreves?: ");
         descricao = entrada.nextLine();
 
-        System.out.format("O seu nome eh %s tens %d anos de idade e pesas %.2f kg e te descreve como sendo alguem %s\n", nome, idade, peso, descricao);
+        System.out.format("O seu nome eh %s tens %d anos de idade e pesas %.2f kg e te descreve como sendo alguem %s\n", nome, idade, peso, descricao); */
     }
 }
